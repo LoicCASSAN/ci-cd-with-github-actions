a89d9396b572d08202060853e62b82088624bea4
LoicCASSAN <loic.cassan@efrei.net>
2024-01-09 12:43:32 +0100

    test fix

--- test-endtoend-app.py
+++ test-endtoend-app.py
@@ -27,10 +27,10 @@ class TestAppE2E(unittest.TestCase):
         # Ajout d'un item
         input_field = self.driver.find_element(By.NAME, 'item')
         input_field.send_keys('Item : Voiture')
-        time.sleep(2)  # Attend 2 seconds  
+        #time.sleep(2)  # Attend 2 seconds  
 
         input_field.send_keys(Keys.RETURN)
-        time.sleep(2)  
+        #time.sleep(2)  
 
         # Vérication ajout de l'item
         self.assertIn('Item : Voiture', self.driver.page_source)
@@ -39,13 +39,13 @@ class TestAppE2E(unittest.TestCase):
         update_links = self.driver.find_elements(By.XPATH, '//a[contains(@href, "/update/")]')
         if update_links:
             update_links[0].click()
-            time.sleep(2)  
+            #time.sleep(2)  
 
         # Updated de l'item
         update_field = self.driver.find_element(By.NAME, 'new_item')
         update_field.send_keys('Item : Voiture Updated')
         update_field.send_keys(Keys.RETURN)
-        time.sleep(2)  
+        #time.sleep(2)  
 
         # Vérification update
         self.assertIn('Item : Voiture Updated', self.driver.page_source)
@@ -54,13 +54,13 @@ class TestAppE2E(unittest.TestCase):
         delete_buttons = self.driver.find_elements(By.XPATH, '//a[contains(@href, "/delete/")]')
         if delete_buttons:
             delete_buttons[0].click()  
-            time.sleep(2)  
+            #time.sleep(2)  
 
         # Vérification suppression
         self.assertNotIn('Item : Voiture Updated', self.driver.page_source)
 
     def tearDown(self):
-        time.sleep(2) 
+        #time.sleep(2) 
         self.driver.close()
 
 if __name__ == '__main__':
